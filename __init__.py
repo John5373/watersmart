3f897a2e20577960ee1acf5e7b95b306ce464773
John5373 <maniscalcojohn@gmail.com>
2025-01-07 09:05:04 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -43,29 +43,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
-
-# File: custom_components/watersmart/sensor.py
-from homeassistant.helpers.entity import Entity
-from . import DOMAIN
-
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up WaterSmart sensors from a config entry."""
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    sensors = [WaterSmartSensor(coordinator, "daily_usage"), WaterSmartSensor(coordinator, "monthly_usage")]
-    async_add_entities(sensors)
-
-class WaterSmartSensor(Entity):
-    def __init__(self, coordinator, sensor_type):
-        self.coordinator = coordinator
-        self.type = sensor_type
-
-    @property
-    def name(self):
-        return f"WaterSmart {self.type}"
-
-    @property
-    def state(self):
-        return self.coordinator.data.get(self.type)
-
-    async def async_update(self):
-        await self.coordinator.async_request_refresh()
